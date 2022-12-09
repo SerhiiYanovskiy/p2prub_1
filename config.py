d870ec0bce370d46fa34f52b7f16405870c00e28
serhii <https://github.com/SerhiiYanovskiy/socar.git>
2022-12-09 12:38:08 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+SPREADSHEET_ID = "1GCKQ-dEZYb44oiF-Nd43BVMHvXMBRxoVL17QjC1Hm_w"
+SPOT = ["USDT", "BTC", "BUSD", "BNB","RUB", "ETH"]#",
+BANKS_RUB = ["RaiffeisenBank", "YandexMoneyNew","RosBankNew","TinkoffNew", "QIWI","PostBankNew","HomeCreditBank","MTSBank","Payeer","UralsibBank","Advcash"]
+
+LIMITS_RUB = [1000, 5000, 10000, 20000, 30000, 50000, 100000, 200000, 300000, 500000, "Без указания среза"]
+
+headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Content-Length": "123",
+    "content-type": "application/json",
+    "Host": "p2p.binance.com",
+    "Origin": "https://p2p.binance.com",
+    "Pragma": "no-cache",
+    "TE": "Trailers",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"}
